f1d481ba3af8fec454587005e4dbcb0cd0a09d56
Edgar López <lopezedg9@gmail.com>
2016-08-30 23:25:37 -0700

    Add files via upload
    Conexion a la base de Datos Mysql con python 2.7.x

---
+++ conexion.py
@@ -0,0 +1,24 @@
+import MySQLdb
+ 
+DB_HOST = 'localhost' 
+DB_USER = 'root' 
+DB_PASS = 'root' 
+DB_NAME = 'prueba' 
+ 
+def run_query(query=''): 
+    datos = [DB_HOST, DB_USER, DB_PASS, DB_NAME] 
+ 
+    conn = MySQLdb.connect(*datos) # Conectar a la base de datos 
+    cursor = conn.cursor()         # Crear un cursor 
+    cursor.execute(query)          # Ejecutar una consulta 
+ 
+    if query.upper().startswith('SELECT'): 
+        data = cursor.fetchall()   # Traer los resultados de un select 
+    else: 
+        conn.commit()              # Hacer efectiva la escritura de datos 
+        data = None 
+ 
+    cursor.close()                 # Cerrar el cursor 
+    conn.close()                   # Cerrar la conexión 
+ 
+    return data
